2f721be57ca2243197a9946494ec5629a286519b
adamoolah_offical <119093751+Adamoolahwastakenwastaken@users.noreply.github.com>
2022-12-09 20:57:29 +0400

    music and sfx (and a few more changes to make it feel better)
    SO I WAS WORKING FOR 6 HOURS THEN I REALIZED I HAD TO PUT MY PLAYER BEFORE THE ZOMBIES 6 HOURS DOWN THE DRAIN

--- Sprites.py
+++ Sprites.py
@@ -1,48 +1,47 @@
 import pygame as pg
-from Settings import *
-from tilemap import *
-from random import uniform, randint, choice
-import pytweening
-
+from random import uniform, choice, randint, random
+from settings import *
+from tilemap import collide_hit_rect
+import pytweening as tween
 vec = pg.math.Vector2
 
 def collide_with_walls(sprite, group, dir):
     if dir == 'x':
-        hits = pg.sprite.spritecollide(sprite, group, False, collide_with_hit_rect)
+        hits = pg.sprite.spritecollide(sprite, group, False, collide_hit_rect)
         if hits:
-            if sprite.vel.x > 0:
+            if hits[0].rect.centerx > sprite.hit_rect.centerx:
                 sprite.pos.x = hits[0].rect.left - sprite.hit_rect.width / 2
-            if sprite.vel.x < 0:
+            if hits[0].rect.centerx < sprite.hit_rect.centerx:
                 sprite.pos.x = hits[0].rect.right + sprite.hit_rect.width / 2
             sprite.vel.x = 0
             sprite.hit_rect.centerx = sprite.pos.x
     if dir == 'y':
-        hits = pg.sprite.spritecollide(sprite, group, False, collide_with_hit_rect)
+        hits = pg.sprite.spritecollide(sprite, group, False, collide_hit_rect)
         if hits:
-            if sprite.vel.y > 0:
+            if hits[0].rect.centery > sprite.hit_rect.centery:
                 sprite.pos.y = hits[0].rect.top - sprite.hit_rect.height / 2
-            if sprite.vel.y < 0:
+            if hits[0].rect.centery < sprite.hit_rect.centery:
                 sprite.pos.y = hits[0].rect.bottom + sprite.hit_rect.height / 2
             sprite.vel.y = 0
             sprite.hit_rect.centery = sprite.pos.y
 
-
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
+        self._layer = PLAYER_LAYER
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self._layer = PLAYER_LAYER
-        self.image = game.player_image
+        self.image = game.player_img
         self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
         self.hit_rect = PLAYER_HIT_RECT
         self.hit_rect.center = self.rect.center
-        self.vel = vec(0,0) 
-        self.rect.center = (x,y)
-        self.pos = vec(x,y)
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y)
         self.rot = 0
         self.last_shot = 0
         self.health = PLAYER_HEALTH
+
     def get_keys(self):
         self.rot_speed = 0
         self.vel = vec(0, 0)
@@ -52,9 +51,9 @@ class Player(pg.sprite.Sprite):
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
             self.rot_speed = -PLAYER_ROT_SPEED
         if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vel = vec(PLAYERSPEED,0).rotate(-self.rot)
+            self.vel = vec(PLAYER_SPEED, 0).rotate(-self.rot)
         if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vel = vec(-PLAYERSPEED /2,0).rotate(-self.rot)
+            self.vel = vec(-PLAYER_SPEED / 2, 0).rotate(-self.rot)
         if keys[pg.K_SPACE]:
             now = pg.time.get_ticks()
             if now - self.last_shot > BULLET_RATE:
@@ -62,14 +61,14 @@ class Player(pg.sprite.Sprite):
                 dir = vec(1, 0).rotate(-self.rot)
                 pos = self.pos + BARREL_OFFSET.rotate(-self.rot)
                 Bullet(self.game, pos, dir)
-                self.vel = vec(-KICK_BACK).rotate(-self.rot)
+                self.vel = vec(-KICKBACK, 0).rotate(-self.rot)
+                choice(self.game.weapon_sounds['gun']).play()
                 MuzzleFlash(self.game, pos)
-                
 
     def update(self):
         self.get_keys()
         self.rot = (self.rot + self.rot_speed * self.game.dt) % 360
-        self.image = pg.transform.rotate(self.game.player_image, self.rot)
+        self.image = pg.transform.rotate(self.game.player_img, self.rot)
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
         self.pos += self.vel * self.game.dt
@@ -78,20 +77,21 @@ class Player(pg.sprite.Sprite):
         self.hit_rect.centery = self.pos.y
         collide_with_walls(self, self.game.walls, 'y')
         self.rect.center = self.hit_rect.center
+
     def add_health(self, amount):
         self.health += amount
         if self.health > PLAYER_HEALTH:
-            self.health = PLAYER_HEALTH     
+            self.health = PLAYER_HEALTH
 
 class Mob(pg.sprite.Sprite):
-
     def __init__(self, game, x, y):
+        self._layer = MOB_LAYER
         self.groups = game.all_sprites, game.mobs
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self._layer = MOB_LAYER
-        self.image = game.Mob_image
+        self.image = game.mob_img.copy()
         self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
         self.hit_rect = MOB_HIT_RECT.copy()
         self.hit_rect.center = self.rect.center
         self.pos = vec(x, y)
@@ -100,31 +100,37 @@ class Mob(pg.sprite.Sprite):
         self.rect.center = self.pos
         self.rot = 0
         self.health = MOB_HEALTH
-   
+        self.speed = choice(MOB_SPEEDS)
+        self.target = game.player
 
     def avoid_mobs(self):
         for mob in self.game.mobs:
             if mob != self:
                 dist = self.pos - mob.pos
                 if 0 < dist.length() < AVOID_RADIUS:
-                    self.acc += dist.normalize()    
+                    self.acc += dist.normalize()
+
     def update(self):
-        self.rot = (self.game.player.pos - self.pos).angle_to(vec(1, 0))
-        self.image = pg.transform.rotate(self.game.Mob_image, self.rot)
-        self.rect = self.image.get_rect()
-        self.rect.center = self.pos
-        self.acc = vec(1, 0).rotate(-self.rot)
-        self.avoid_mobs()
-        self.acc.scale_to_length(MOB_SPEED)
-        self.acc += self.vel * -1
-        self.vel += self.acc * self.game.dt
-        self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
-        self.hit_rect.centerx = self.pos.x
-        collide_with_walls(self, self.game.walls, 'x')
-        self.hit_rect.centery = self.pos.y
-        collide_with_walls(self, self.game.walls, 'y')
-        self.rect.center = self.hit_rect.center
+        target_dist = self.target.pos - self.pos
+        if target_dist.length_squared() < DETECT_RADIUS**2:
+            if random() < 0.002:
+                choice(self.game.zombie_moan_sounds).play()
+            self.rot = target_dist.angle_to(vec(1, 0))
+            self.image = pg.transform.rotate(self.game.mob_img, self.rot)
+            self.rect.center = self.pos
+            self.acc = vec(1, 0).rotate(-self.rot)
+            self.avoid_mobs()
+            self.acc.scale_to_length(self.speed)
+            self.acc += self.vel * -1
+            self.vel += self.acc * self.game.dt
+            self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
+            self.hit_rect.centerx = self.pos.x
+            collide_with_walls(self, self.game.walls, 'x')
+            self.hit_rect.centery = self.pos.y
+            collide_with_walls(self, self.game.walls, 'y')
+            self.rect.center = self.hit_rect.center
         if self.health <= 0:
+            choice(self.game.zombie_hit_sounds).play()
             self.kill()
 
     def draw_health(self):
@@ -141,57 +147,26 @@ class Mob(pg.sprite.Sprite):
 
 class Bullet(pg.sprite.Sprite):
     def __init__(self, game, pos, dir):
+        self._layer = BULLET_LAYER
         self.groups = game.all_sprites, game.bullets
         pg.sprite.Sprite.__init__(self, self.groups)
-        self.image = game.Bullet_image
-        self._layer = BULLET_LAYER
-        self.rect = self.image.get_rect()
-        self.pos = vec(pos)
-        self.hit_rect = self.rect
         self.game = game
+        self.image = game.bullet_img
+        self.rect = self.image.get_rect()
+        self.hit_rect = self.rect
+        self.pos = vec(pos)
         self.rect.center = pos
-        spread = uniform(-GUN_SPREAD,GUN_SPREAD)
+        spread = uniform(-GUN_SPREAD, GUN_SPREAD)
         self.vel = dir.rotate(spread) * BULLET_SPEED
         self.spawn_time = pg.time.get_ticks()
+
     def update(self):
         self.pos += self.vel * self.game.dt
         self.rect.center = self.pos
-        if pg.sprite.spritecollideany(self,self.game.walls):
+        if pg.sprite.spritecollideany(self, self.game.walls):
             self.kill()
-        if pg.time.get_ticks () - self.spawn_time > BULLET_LIFE:
+        if pg.time.get_ticks() - self.spawn_time > BULLET_LIFETIME:
             self.kill()
-# ill put it if i like
-
-#class Wall(pg.sprite.Sprite):
-    #def __init__(self, game, x, y):
-        #self.groups = game.all_sprites, game.walls
-        #pg.sprite.Sprite.__init__(self, self.groups)
-        #self.game = game
-        #self.image = game.wall_image
-
-        #self.rect = self.image.get_rect()
-        #self.x = x
-        #self.y = y
-        #self.rect.x = x * TILESIZE
-        #self.rect.y = y * TILESIZE
-
-class MuzzleFlash(pg.sprite.Sprite):
-    def __init__(self, game, pos):
-        self.groups = game.all_sprites
-        self._layer = EFFECTS_LAYER
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        size = randint(20, 50)
-        self.image = pg.transform.scale(choice(game.Muzzle_images), (size, size))
-        self.rect = self.image.get_rect()
-        self.pos = pos
-        self.rect.center = pos
-        self.spawn_time = pg.time.get_ticks()
-
-    def update(self):
-        if pg.time.get_ticks() - self.spawn_time > MUZZLE_DURATION:
-            self.kill()
-
 
 class Obstacle(pg.sprite.Sprite):
     def __init__(self, game, x, y, w, h):
@@ -205,24 +180,41 @@ class Obstacle(pg.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
+class MuzzleFlash(pg.sprite.Sprite):
+    def __init__(self, game, pos):
+        self._layer = EFFECTS_LAYER
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        size = randint(20, 50)
+        self.image = pg.transform.scale(choice(game.gun_flashes), (size, size))
+        self.rect = self.image.get_rect()
+        self.pos = pos
+        self.rect.center = pos
+        self.spawn_time = pg.time.get_ticks()
+
+    def update(self):
+        if pg.time.get_ticks() - self.spawn_time > FLASH_DURATION:
+            self.kill()
+
 class Item(pg.sprite.Sprite):
     def __init__(self, game, pos, type):
+        self._layer = ITEMS_LAYER
         self.groups = game.all_sprites, game.items
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self._layer = ITEM_IMAGES
         self.image = game.item_images[type]
         self.rect = self.image.get_rect()
         self.type = type
-        self.rect.center = pos
         self.pos = pos
-        self.tween = pytweening.easeInOutSine
+        self.rect.center = pos
+        self.tween = tween.easeInOutSine
         self.step = 0
-        self.dir = 1 
+        self.dir = 1
 
     def update(self):
-        #making cool epic gamer animation
-        offset = BOB_RANGE * (self.tween(self.step / BOB_RANGE) -0.5)
+        # bobbing motion
+        offset = BOB_RANGE * (self.tween(self.step / BOB_RANGE) - 0.5)
         self.rect.centery = self.pos.y + offset * self.dir
         self.step += BOB_SPEED
         if self.step > BOB_RANGE:
--- tilemap.py
+++ tilemap.py
@@ -1,38 +1,22 @@
-import pygame
-from Settings import *
+import pygame as pg
 import pytmx
+from settings import *
 
-def collide_with_hit_rect(one,two):
+def collide_hit_rect(one, two):
     return one.hit_rect.colliderect(two.rect)
+
 class Map:
-    def __init__(self,fileName):
+    def __init__(self, filename):
         self.data = []
-        with open(fileName, 'rt') as f:
+        with open(filename, 'rt') as f:
             for line in f:
-                self.data.append(line)
+                self.data.append(line.strip())
+
         self.tilewidth = len(self.data[0])
-        self.tileheight = len(self.data) 
+        self.tileheight = len(self.data)
         self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
-class Camera:
-    def __init__(self,width,height):
-        self.camera = pygame.Rect(0,0,width,height)
-        self.width = width
-        self.height = height
-    def apply(self,entity):
-        return entity.rect.move(self.camera.topleft)
 
-    def apply_rect(self,rect):
-        return rect.move(self.camera.topleft)    
-    def update(self,target):
-        x = -target.rect.centerx + int(WIDTH/2)
-        y = -target.rect.centery + int(HEIGHT /2)
-        # limit Camera Bounds
-        x = min(0,x) # left side
-        y = min(0,y) # top side
-        x = max(-(self.width - WIDTH),x)
-        y = max(-(self.height - HEIGHT),y)
-        self.camera = pygame.Rect(x,y,self.width,self.height)
 class TiledMap:
     def __init__(self, filename):
         tm = pytmx.load_pygame(filename, pixelalpha=True)
@@ -51,6 +35,29 @@ class TiledMap:
                                             y * self.tmxdata.tileheight))
 
     def make_map(self):
-        temp_surface = pygame.Surface((self.width,self.height))
+        temp_surface = pg.Surface((self.width, self.height))
         self.render(temp_surface)
         return temp_surface
+
+class Camera:
+    def __init__(self, width, height):
+        self.camera = pg.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+
+    def apply(self, entity):
+        return entity.rect.move(self.camera.topleft)
+
+    def apply_rect(self, rect):
+        return rect.move(self.camera.topleft)
+
+    def update(self, target):
+        x = -target.rect.centerx + int(WIDTH / 2)
+        y = -target.rect.centery + int(HEIGHT / 2)
+
+        # limit scrolling to map size
+        x = min(0, x)  # left
+        y = min(0, y)  # top
+        x = max(-(self.width - WIDTH), x)  # right
+        y = max(-(self.height - HEIGHT), y)  # bottom
+        self.camera = pg.Rect(x, y, self.width, self.height)
